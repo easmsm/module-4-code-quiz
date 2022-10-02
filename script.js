6561bcf930be92339c8c15d7ea3b87d6a7ad9b52
ekukwa <easmsm@gmail.com>
2022-10-02 19:31:49 -0400

    added pseudo code to Javascript file

---
+++ script.js
@@ -0,0 +1,18 @@
+// variables that apply to the whole page
+
+//questions
+
+//button answer option displays
+
+//answer variables that will be attached to each question
+
+//timer
+
+//timer score functions (add time for correct answers, remove time for incorrect answers)
+
+//high score storage
+
+//adding score to high score storage
+
+//correct or incorrect answer
+
