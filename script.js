a97f9e28c2a9eb38bce37ad8c25795d2b44c2cfa
ekukwa <easmsm@gmail.com>
2022-10-02 20:20:06 -0400

    added first round of variables based on pseudo code planning

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-// variables that apply to the whole page
+// variables 
+
+//variables for timer/score keeping
+var score = 0; // Max value by decreasing each wrong answer
+var highScore = 50; // highest possible score with the number of questions I'll be adding
+var checkTimes = 1 //  last question check
+var htmlTimeLeft = document.getElementById('timeLeft'); 
+var enterInitialsPrompt = document.createElement("enterInitialsPrompt"); 
+var enterInitialsHere = document.createElement("enterInitialsHere"); 
+
+//answer buttons
+var answerOption1Btn = document.getElementById('answeroption1'); 
+var answerOption2Btn = document.getElementById('answeroption2');
+var answerOption3Btn = document.getElementById('answeroption3'); 
+var answerOption4Btn = document.getElementById('answeroption4');
+
+//question and answer variables
+var quizStatus = true; // Know the status of the quiz. Quiz is not running = false , running = true
+var questionNumber = 0; 
+var answerNumber = 0; 
+var finalAnswerCheck = 0  
+var questionsEl = document.getElementById('questions'); 
+
+//header variables
+var viewScoresBtn = document.getElementById('viewScores');
+
+//quiz body structure/other variables
+var startQuizBtn = document.getElementById('startQuiz');
+var submitScore = document.getElementById('submitScore'); 
+var mainDiv = document.getElementById('main'); 
+// Display counter 
+var answerRightWrong = document.getElementById('answerRightWrong'); 
+var questionHere = document.createElement("questionHere"); 
+var finalScore = document.createElement("finalScore"); 
+
+
 
 //questions
 
