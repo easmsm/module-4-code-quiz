d28aba2a91f3315f67b89534bf39795f646b1f6f
ekukwa <easmsm@gmail.com>
2022-10-02 20:55:46 -0400

    added questions and answers. This has an error, but I'll come back to it once I can see it in the browser

--- script.js
+++ script.js
@@ -45,7 +45,55 @@ submitScore.style.display = 'none';
 answerRightWrong.style.display='none';
 enterInitialsHere.style.display='none';
 
-//questions
+//questions and answers
+
+// SOMETHING IS WRONG HERE, COME BACK AND FIX THE ORDER OF THE CORRECT ANSWERS
+// Object that holds correct answers with the print out of the question. The place of the correct answer in the array of answer options for each question is listed first (in order at the moment, hope to vary this in the future)
+var questions = { 
+    correct: { 
+        0 : "What number is used for the first position in an array",
+        1 : "Which of the following is not a programming language?",
+        2 : "Where do you link your JavaScript on your HTML file?", 
+        3 : "What are variables?", 
+        4 : "Which of the following is an example of a string?"
+    }
+};
+
+//All answers
+var answers = { 
+    answers: { 
+        0 : {
+            0: "0",
+            1: "1",
+            2: "2",
+            3: "None of the Above"},
+        1 : {
+            0: "Java",
+            1: "Jaba",
+            2: "JavaScript",
+            3: "Python"},
+        2 : { 
+            0: "In the header",
+            1: "In the footer",
+            2: "At the bottom of the file, right before the close of the body tag", 
+            3: "At the top of the file, after the html language is designated"},      
+        3 : { 
+            0: "Placeholders for numbers",
+            1: "Where deleted information is stored",
+            2: "Text input spaces for end users", 
+            3: "Where information is stored"},      
+        4 : { 
+            0: "Hello",
+            1: "'Where's Waldo?'",
+            2: "!Hello",
+            3: "!Where's Waldo?"},  
+    }
+};
+
+//end questions and answers section
+
+
+
 
 //button answer option displays
 
